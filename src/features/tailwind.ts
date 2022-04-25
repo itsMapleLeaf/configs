34168047dd58764c333eb06f2272b1cecafdb12a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-04-25 11:33:51 -0500

    simplify tailwind feature

--- src/features/tailwind.ts
+++ src/features/tailwind.ts
@@ -1,17 +1,9 @@
 import type { Feature } from "../feature.js"
-import type { ProjectContext } from "../project-context.js"
 
 export const tailwindFeature: Feature = {
   name: "Tailwind CSS (Styling)",
   initiallyChecked: (context) =>
     context.environment.isBrowser && context.environment.isApplication,
-  installDevDependencies: () => ["tailwindcss", "postcss"],
-  copyFiles: () => [
-    { source: "assets/tailwindcss/*", destination: "." },
-    { source: "assets/tailwindcss/src/*", destination: "src" },
-  ],
-}
-
-export function isTailwindEnabled(context: ProjectContext) {
-  return context.enabledFeatures.includes(tailwindFeature)
+  installDevDependencies: () => ["tailwindcss", "postcss", "autoprefixer"],
+  copyFiles: () => [{ source: "assets/tailwindcss/*", destination: "." }],
 }
