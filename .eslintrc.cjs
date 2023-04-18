ed76534d93e50e035598b496a5de6f6674457024
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-17 21:48:37 -0500

    use package.json exports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: [require.resolve("./eslint")],
+  extends: [require.resolve("./.eslintrc.base.cjs")],
   ignorePatterns: [
     "**/node_modules/**",
     "**/dist/**",
