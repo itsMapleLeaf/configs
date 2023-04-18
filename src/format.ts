797fc1fb391f1c2b1a0423425965f3f3c4e0c13a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-17 21:49:50 -0500

    fix prettier import

--- src/format.ts
+++ src/format.ts
@@ -1,5 +1,5 @@
 import prettier from "prettier"
-import prettierConfig from "../prettier/index.cjs"
+import prettierConfig from "../.prettierrc.cjs"
 
 export function formatWithPrettier(text: string, filename: string) {
   return prettier.format(text, { ...prettierConfig, filepath: filename })
