fa76fa1929ed7eec02b2cd0b458e6968df49db08
Darius <19603573+itsMapleLeaf@users.noreply.github.com>
2023-09-08 13:27:55 -0500

    enable tabs in prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 export default {
 	semi: false,
-	trailingComma: "all",
+	trailingComma: "all",useTabs: true,
 	plugins: [
 		"prettier-plugin-jsdoc",
 		"prettier-plugin-astro",
