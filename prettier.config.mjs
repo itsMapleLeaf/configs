006d56c436848a1e91948ca375c863bc3627c9e2
itsMapleLeaf <itsMapleLeaf@users.noreply.github.com>
2023-09-08 18:28:29 +0000

    ci: apply formatting changes

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import("prettier").Config} */
 export default {
 	semi: false,
-	trailingComma: "all",useTabs: true,
+	trailingComma: "all",
+	useTabs: true,
 	plugins: [
 		"prettier-plugin-jsdoc",
 		"prettier-plugin-astro",
