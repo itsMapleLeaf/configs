1d965dff12905bd0224deedbe1874e957b490e2b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-09-07 19:07:43 -0500

    setup biome

--- eslint.config.react.cjs
+++ eslint.config.react.cjs
@@ -1,16 +1,16 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  plugins: ["react"],
-  settings: {
-    react: { version: "detect" },
-  },
-  overrides: [
-    {
-      files: ["*.{tsx,jsx}"],
-      extends: ["plugin:react/recommended", "plugin:react/jsx-runtime"],
-      rules: {
-        "react/prop-types": "off",
-      },
-    },
-  ],
+	plugins: ["react"],
+	settings: {
+		react: { version: "detect" },
+	},
+	overrides: [
+		{
+			files: ["*.{tsx,jsx}"],
+			extends: ["plugin:react/recommended", "plugin:react/jsx-runtime"],
+			rules: {
+				"react/prop-types": "off",
+			},
+		},
+	],
 }
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-  semi: false,
-  trailingComma: "all",
-  plugins: [
-    "prettier-plugin-jsdoc",
-    "prettier-plugin-astro",
-    "prettier-plugin-tailwindcss",
-  ],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	semi: false,
+	trailingComma: "all",
+	plugins: [
+		"prettier-plugin-jsdoc",
+		"prettier-plugin-astro",
+		"prettier-plugin-tailwindcss",
+	],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 }
