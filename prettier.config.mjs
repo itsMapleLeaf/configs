e208340bbfcf66c36e4141f01444ca689a2baba0
Darius <19603573+itsMapleLeaf@users.noreply.github.com>
2023-09-11 16:24:25 -0500

    don't use tabs on package.json
    package managers keep writing this with spaces, so we won't fight with them

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -11,6 +11,12 @@ export default {
 	],
 	overrides: [
 		{
+			files: "package.json",
+			options: {
+				useTabs: false,
+			},
+		},
+		{
 			files: "*.astro",
 			options: {
 				parser: "astro",
