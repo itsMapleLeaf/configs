0a0871d1c0f979aa78545278070acc06d775ad10
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-09-28 13:30:50 -0500

    add tailwind extensions

---
+++ tailwind-extensions.mjs
@@ -0,0 +1,37 @@
+import containerQueries from "@tailwindcss/container-queries"
+import plugin from "tailwindcss/plugin"
+
+/** @satisfies {Partial<import("tailwindcss").Config>} */
+const preset = {
+  theme: {
+    extend: {
+      minWidth: (utils) => utils.theme("width"),
+      maxWidth: (utils) => utils.theme("width"),
+      minHeight: (utils) => utils.theme("height"),
+      maxHeight: (utils) => utils.theme("height"),
+    },
+  },
+  plugins: [
+    containerQueries,
+    plugin(function fluidCols(api) {
+      api.matchUtilities(
+        {
+          "fluid-cols": (value) => ({
+            "grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))`,
+          }),
+        },
+        { values: api.theme("width") },
+      )
+    }),
+    plugin(function size(api) {
+      api.matchUtilities(
+        { s: (value) => ({ width: value, height: value }) },
+        { values: { ...api.theme("width"), ...api.theme("height") } },
+      )
+    }),
+    plugin(function ariaCurrentPage(api) {
+      api.addVariant("aria-current-page", '&[aria-current="page"]')
+    }),
+  ],
+}
+export default preset
