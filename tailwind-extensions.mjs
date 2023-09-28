71a805c4090b7f8f2908e955dbe5352412ce4a02
itsMapleLeaf <itsMapleLeaf@users.noreply.github.com>
2023-09-28 18:47:15 +0000

    ci: apply formatting changes

--- tailwind-extensions.mjs
+++ tailwind-extensions.mjs
@@ -3,36 +3,36 @@ import plugin from "tailwindcss/plugin"
 
 /** @satisfies {import("tailwindcss").Config} */
 const preset = {
-  content: [],
-  theme: {
-    extend: {
-      minWidth: (utils) => utils.theme("width"),
-      maxWidth: (utils) => utils.theme("width"),
-      minHeight: (utils) => utils.theme("height"),
-      maxHeight: (utils) => utils.theme("height"),
-    },
-  },
-  plugins: [
-    containerQueries,
-    plugin(function fluidCols(api) {
-      api.matchUtilities(
-        {
-          "fluid-cols": (value) => ({
-            "grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))`,
-          }),
-        },
-        { values: api.theme("width") },
-      )
-    }),
-    plugin(function size(api) {
-      api.matchUtilities(
-        { s: (value) => ({ width: value, height: value }) },
-        { values: { ...api.theme("width"), ...api.theme("height") } },
-      )
-    }),
-    plugin(function ariaCurrentPage(api) {
-      api.addVariant("aria-current-page", '&[aria-current="page"]')
-    }),
-  ],
+	content: [],
+	theme: {
+		extend: {
+			minWidth: (utils) => utils.theme("width"),
+			maxWidth: (utils) => utils.theme("width"),
+			minHeight: (utils) => utils.theme("height"),
+			maxHeight: (utils) => utils.theme("height"),
+		},
+	},
+	plugins: [
+		containerQueries,
+		plugin(function fluidCols(api) {
+			api.matchUtilities(
+				{
+					"fluid-cols": (value) => ({
+						"grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))`,
+					}),
+				},
+				{ values: api.theme("width") },
+			)
+		}),
+		plugin(function size(api) {
+			api.matchUtilities(
+				{ s: (value) => ({ width: value, height: value }) },
+				{ values: { ...api.theme("width"), ...api.theme("height") } },
+			)
+		}),
+		plugin(function ariaCurrentPage(api) {
+			api.addVariant("aria-current-page", '&[aria-current="page"]')
+		}),
+	],
 }
 export default preset
